75ea1152b58a41da76386f37ac0a1caaea3cd0ba
thara-messeroux <tharamesseroux@gmail.com>
2026-03-01 18:54:58 -0500

    Add User model and sign-up POST with bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
+// controllers/auth.js
 const express = require("express");
+// Import method-override for PUT and DELETE requests
 const router = express.Router();
+// Import bcrypt for password hashing
+const bcrypt = require("bcrypt");
+//  Import User model
+const User = require("../models/User");
 
 // GET sign up page
 router.get("/sign-up", (req, res) => {
@@ -11,4 +17,36 @@ router.get("/sign-in", (req, res) => {
     res.render("auth/sign-in.ejs");
 });
 
-module.exports = router;
+// POST sign up form
+router.post("/sign-up", async (req, res) => {
+    try {
+        const { username, password, confirmPassword } = req.body;
+
+        // 1) confirm password match
+        if (password !== confirmPassword) {
+            return res.send("Password and Confirm Password must match");
+        }
+
+        // 2) username must be unique
+        const userInDatabase = await User.findOne({ username });
+        if (userInDatabase) {
+            return res.send("Username already taken");
+        }
+
+        // 3) hash password
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        req.body.password = hashedPassword;
+
+        // 4) create user
+        await User.create(req.body);
+
+        // 5) redirect somewhere (landing page)
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.send("Something went wrong during sign up");
+    }
+});
+
+// POST sign in form
+ module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 // models/User.js
 const mongoose = require("mongoose");
 
+// Create user schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
 });
 
-module.exports = mongoose.model("User", userSchema);
+// Create user model
+const User = mongoose.model("User", userSchema);
+
+// Export user model
+module.exports = User;
+
